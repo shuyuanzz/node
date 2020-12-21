3b3cfdf9727a691a2c6e8a6aca8b592898f99fdb
shuyuan <seanshu96@gmail.com>
2020-12-21 16:12:10 +0800

    lesson: http 服务的实现

---
+++ http/index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const fs = require('fs');
+http.createServer(function(req,res) {
+    if(request = '/favicon.ico') {
+        res.writeHead(200);
+        res.end();
+        return;
+    }
+    res.writeHead(200);
+    fs.createReadStream(__dirname + '/index.html').pipe(res);
+}).listen(3000)
